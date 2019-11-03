dbd329daf7ad42212df708b98e263e650695693c
monabart <bartlingmo@sbg.ac.at>
2019-11-03 15:44:34 -0500

    update

--- Archiv/Hochzeitsmesse_Einladung/assets/standesamt.js
+++
@@ -1,8 +0,0 @@
-var standesamt = {
-"type": "FeatureCollection",
-"name": "location_pt",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "id": 4, "location": "Püttenhus Dötlingen", "url": "assets/puttenhus.jpg" }, "geometry": { "type": "Point", "coordinates": [ 8.382388, 52.934085 ] } }
-]
-}
--- Archiv/Scrollytelling/assets/kirche.js
+++
@@ -1,8 +0,0 @@
-var kirche = {
-"type": "FeatureCollection",
-"name": "location_pt",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "id": 1, "location": "Kirche Dötlingen", "url": "https://img.nwzonline.de/rf/image_online/NWZ_CMS/NWZ/2017-2020/Produktion/2018/06/30/LANDKREIS/3/Bilder/LANDKREIS_3_5e884201-bad2-4083-b4d3-95d5aff506e2--438x337.jpg" }, "geometry": { "type": "Point", "coordinates": [ 8.379130, 52.935258 ] } }
-]
-}
